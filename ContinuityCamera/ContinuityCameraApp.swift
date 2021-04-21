48526121705cdf849f1b2c369472c7da67f8acce
Philipp <pd95@users.noreply.github.com>
2021-04-21 18:24:11 +0200

    Initial Commit

---
+++ ContinuityCamera/ContinuityCameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContinuityCameraApp.swift
+//  ContinuityCamera
+//
+//  Created by Philipp on 21.04.21.
+//
+
+import SwiftUI
+
+@main
+struct ContinuityCameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
